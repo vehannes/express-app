dbccefcb47c838399a296017d736c3ac21729359
Juha Vepsä <juha.a.vepsa@gmail.com>
2024-05-31 19:20:41 +0300

    Change the message.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Does this watchtower thing work?</h1>')
+  res.send('<h1>Hello from render!</h1>')
 })
 
 const PORT = 8080
